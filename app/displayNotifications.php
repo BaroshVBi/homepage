3d7d7cd822d693ccac70ea19e41121eb5d0ccfe3
BaroshVBi <gbartek123@gmail.com>
2023-06-06 15:05:30 +0200

    Update displayNotifications.php

--- app/displayNotifications.php
+++ app/displayNotifications.php
@@ -9,11 +9,11 @@
 		while ($row = $result->fetch_assoc()) {
 			echo "<div class='notify_tab'>" . $row['id'] . $row['user_id'] . $row['title'] . $row['descr'] . $row['date'] . $row['time'] . "</div>";
 		}
-		echo "</div></div><div id='notify_tab_controls' class='col-12'><div class='col-1 center cursor_pointer' onclick=''>&#10094;</div>";
+		echo "</div></div><div id='notify_tab_controls' class='col-12'><div class='col-1 cursor_pointer' onclick=''>&#10094;</div>";
 		for($i = 1; $i <= $count; $i++ ){
-			echo "<div class='col-1 center cursor_pointer notify_tab_dot' onClick='notifyTab(this);'>O</div>";
+			echo "<div class='col-1 cursor_pointer notify_tab_dot' onClick='notifyTab(this);'>O</div>";
 		}
-		echo "<div class='col-1 center cursor_pointer' onclick=''>&#10095;</div></div>";
+		echo "<div class='col-1 cursor_pointer' onclick=''>&#10095;</div></div>";
 	} 
 	else {
 		echo "<div style='text-align:center;'>Brak Wydarzeń</div>";
