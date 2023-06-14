79883ab4c741ec1f492f5ca4f7ba3d71a4ce48b7
BaroshVBi <gbartek123@gmail.com>
2023-06-14 11:41:08 +0200

    poprawione logowanie ldap

--- app/login.php
+++ app/login.php
@@ -6,65 +6,6 @@
 		<meta http-equiv='Cache-control' content='no-store'>
 	</head>
 	<body>
-		<?php
-			include("config.php");
-			session_start();
-		   
-			if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username']) && isset($_POST['password']) ) {
-				if ($_POST['username'] != "" && $_POST['password'] != ""){
-					$username = $_POST['username'];
-					$password = $_POST['password']; 
-					$ds = ldap_connect($ldap_server);
-					ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-					ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
-					
-					if ($ldap_admin_bind = ldap_bind($ds, $ldap_admin_dn, $ldap_admin_password)) {
-						echo("Admin Login correct<br/>");
-						
-						$filter = '(sAMAccountName=' . $username . ')';
-						$attributes = array("name", "mail", "samaccountname");
-						$result = ldap_search($ds, $ldap_root_dn, $filter, $attributes);
-						
-						$entries = ldap_get_entries($ds, $result);  
-						$ldap_user_dn = $entries[0]["name"][0];
-						$ldap_user_email = $entries[0]["mail"][0];
-						
-						if ($ldap_user_bind = ldap_bind($ds, $ldap_user_dn, $password)) {
-							echo "User Login Correct<br/>"; //Logged in.
-							
-							$sql = "SELECT id FROM users WHERE (user_login = '" . $username . "')";
-							$result = mysqli_query($db,$sql);
-							$count = mysqli_num_rows($result);
-							
-							if($count > 0) {
-								$sql = "UPDATE users SET user_dn = '" . $ldap_user_dn . "', email = '" . $ldap_user_email . "' WHERE users.user_login = '" . $username . "';";
-								$result = mysqli_query($db,$sql);
-							} else {
-								$sql = "INSERT INTO users (user_login, user_dn, email) VALUES ('" . $username . "', '" . $ldap_user_dn . "', '" . $ldap_user_email . "')";
-								$result = mysqli_query($db,$sql);
-							}
-							
-							// Create Session
-							$_SESSION['user_login'] = $username;
-							$_SESSION['user_dn'] = $ldap_user_dn;
-							
-							// Redirect
-							header("location:eventManager.php");
-						} else {
-							echo "User Login Failed<br/>"; //Incorrect login details.
-							// Delete session
-							// Display Error
-						}
-					} else {
-						echo "Admin Login Failed<br/>";	//Error
-					}
-				} else {
-					echo "Wprowadź login i hasło";
-				}
-			} else {
-				echo "Missing Login Data";
-			}
-		?>
 		<div id="login_form" class="col-2 center">
 			<h2 class="col-12 text_center margin_bottom_2vw">Zaloguj się</h2>
 			<form action="" method="post">
@@ -74,9 +15,92 @@
 					<input type="text" class="col-10" name="username" placeholder="" id="username" required/>
 				</div>
 				<label for="password">Hasło:</label>
-				<div class="col-12 margin_bottom_2vw">
+				<div class="col-12">
 					<input type="image" class="col-2" src="public/icons/padlock.png" tabindex="-1"/>
 					<input type="password" class="col-10" name="password" placeholder="" id="password" required>
+				</div>
+				<div id="login_error" class="col-12">
+					<?php
+						include("config.php");
+						session_start();
+						
+						//https://gist.github.com/heiglandreas/5689592
+						//https://github.com/anthony-b/simple-php-LDAP-Authentication
+						
+						if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username']) && isset($_POST['password']) ) {
+							if ($_POST['username'] != "" && $_POST['password'] != ""){
+								$username = $_POST['username'];
+								$password = $_POST['password']; 
+								
+								$ds = ldap_connect($ldap_server);
+								ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+								ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+								
+								if ($ldap_admin_bind = ldap_bind($ds, $ldap_admin_dn, $ldap_admin_password)) {
+									//echo("Admin Login correct<br/>");
+									
+									$filter = '(sAMAccountName=' . $username . ')';
+									$attributes = array("name", "mail", "samaccountname");
+									$result = ldap_search($ds, $ldap_root_dn, $filter, $attributes);
+									
+									if(! $result) {
+										//brak zbioru danych
+										echo 'Złe hasło lub login1';
+									}
+									else if(1 > ldap_count_entries($ds, $result)){
+										//brak danych w zbiorze
+										echo 'Złe hasło lub login2';
+									}
+									else if(1 < ldap_count_entries($ds, $result)){
+										//Wiecej niz jeden uzytkownik z podanycmi danymi
+										echo 'Złe hasło lub login3';
+									} else {
+										//Znaleziono jednego użytkownika z pasujacymi danymi
+										$entries = ldap_get_entries($ds, $result);
+										$ldap_user_dn = $entries[0]["name"][0];
+										$ldap_user_email = $entries[0]["mail"][0];
+										
+										if (@$ldap_user_bind = ldap_bind($ds, $ldap_user_dn, $password)) {
+											//Logged in.
+											//echo "User Login Correct<br/>";
+											
+											$sql = "SELECT id FROM users WHERE (user_login = '" . $username . "')";
+											$result = mysqli_query($db,$sql);
+											$count = mysqli_num_rows($result);
+											
+											if($count > 0) {
+												//Przy pierwszym logowaniu - dodanie uzytkownika do bazy
+												$sql = "UPDATE users SET user_dn = '" . $ldap_user_dn . "', email = '" . $ldap_user_email . "' WHERE users.user_login = '" . $username . "';";
+												$result = mysqli_query($db,$sql);
+											} else {
+												//Przy kazdym nastepnym - aktualizacja danych
+												$sql = "INSERT INTO users (user_login, user_dn, email) VALUES ('" . $username . "', '" . $ldap_user_dn . "', '" . $ldap_user_email . "')";
+												$result = mysqli_query($db,$sql);
+											}
+											
+											// Create Session
+											$_SESSION['user_login'] = $username;
+											$_SESSION['user_dn'] = $ldap_user_dn;
+											
+											// Redirect
+											header("location:eventManager.php");
+										} else {
+											//Incorrect login details.
+											echo "Złe hasło lub login4";
+										}
+									}
+								} else {
+									//Error
+									//echo "Admin Login Failed<br/>";
+								}
+							} else {
+								//Puste pola
+								echo "Wprowadź login i hasło";
+							}
+						} else {
+							//echo "Missing Login Data";
+						}
+					?>
 				</div>
 				<input type="submit" class="col-12" value="Zaloguj"/>
 			</form>
